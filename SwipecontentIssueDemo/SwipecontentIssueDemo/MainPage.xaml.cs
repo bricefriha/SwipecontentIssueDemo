6eaad85d715558d50ca262c17112f5e3be4320f7
Brice Friha <brice.friha@aol.fr>
2020-01-29 23:02:45 +0100

    Add project files.

---
+++ SwipecontentIssueDemo/SwipecontentIssueDemo/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SwipecontentIssueDemo
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Clicked(object sender, EventArgs e)
+        {
+            // Send a display alerte to my god
+            DisplayAlert("From Brice:", "Congrat!! you fix my issue, can I hug you?","YAAAA!");
+        }
+    }
+}
